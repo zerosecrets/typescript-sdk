97d01b526cc2fc002c16eabfaf114d449c73e51d
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-01-31 14:52:01 +0700

    feat: one-liner zero function call

--- README.md
+++ README.md
@@ -1,35 +1,33 @@
 # Zero TypeScript SDK
+
 Typescript SDK for [Zero](https://tryzero.com). Provides a clear and simple interface for the secrets manager GraphQL API.
 
 ## Installation
+
 ```sh
 npm i @zerosecrets/zero
 ```
 
 ## Usage
+
 Fetch secrets for AWS by passing your `zero` token
 
-```typescript
+```ts
 import {zero} from '@zerosecrets/zero'
 
 export async function main() {
   if (!process.env.ZERO_TOKEN) {
     throw new Error('Did you forget to set the ZERO_TOKEN environment variable?')
   }
-  
-  let result
 
-  try {
-    result = await zero({
-      token: process.env.ZERO_TOKEN,
-      pick: ['aws'],
-    }).fetch()
-  } catch(error) {
-    console.error(error)
-  }
+  const [result, error] = await zero({
+    token: process.env.ZERO_TOKEN,
+    pick: ['aws'],
+  })
 
-  console.log(result?.aws) // {secret: "value", secret2: "value2"}
+  result ? console.log(result?.aws) : // {secret: "value", secret2: "value2"}
+  throw new Error(error)
 }
 
-main().catch(console.error)
+main()
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,34 @@
-import {GraphQLClient} from 'graphql-request'
-import {Secrets} from './graphql/secrets'
-import {ResponseBody} from './types'
+import { GraphQLClient } from 'graphql-request'
+import { Secrets } from './graphql/secrets'
+import { ResponseBody } from './types'
 const GRAPHQL_ENDPOINT_URL = 'https://core.tryzero.com/v1/graphql'
 
-export const zero = (params: {pick: Array<string>; token: string, callerName?: string}) => {
+interface IData {
+  [key: string]: { [key: string]: string } | undefined
+}
+
+export const zero = async (params: { pick: Array<string>; token: string, callerName?: string }) => {
   if (typeof params.token === 'undefined' || params.token.length === 0) {
-    throw new Error('Zero token should be non-empty string')
+    const errorMsg = 'Zero token should be non-empty string'
+    return [null, errorMsg]
   }
 
   const client = new GraphQLClient(GRAPHQL_ENDPOINT_URL, {
     headers: {},
-  })
+  });
 
-  return {
-    async fetch(): Promise<{[key: string]: {[key: string]: string} | undefined}> {
-      const response = await client.request<ResponseBody>(Secrets, params)
+  const response = await client.request<ResponseBody>(Secrets, params)
 
-      if(response.errors) {
-        throw new Error(response.errors[0].message)
+    if (response.errors) {
+      return [null, response.errors[0].message]
+    }
+
+    return [response.secrets.reduce((secretAccumulator, secret) => {
+      return {
+        ...secretAccumulator,
+        [secret.name]: secret.fields.reduce((fieldAccumulator, field) => {
+          return { ...fieldAccumulator, [field.name]: field.value }
+        }, {}),
       }
-
-      return response.secrets.reduce((secretAccumulator, secret) => {
-        return {
-          ...secretAccumulator,
-          [secret.name]: secret.fields.reduce((fieldAccumulator, field) => {
-            return {...fieldAccumulator, [field.name]: field.value}
-          }, {}),
-        }
-      }, {})
-    },
-  }
+    }, {}), null]
 }
