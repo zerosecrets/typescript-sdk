244032706236274cac70090f964bf2ec97a7c90b
Andrew Pahomov <andrey.pakhomov@ottofeller.com>
2024-11-22 16:39:35 +0800

    update google refresh token

--- src/refresh-token/refresh-google-tokens.ts
+++ src/refresh-token/refresh-google-tokens.ts
@@ -1,3 +1,4 @@
+import {Google} from 'arctic'
 import {ResponseRefreshTokens} from 'sdk/types'
 
 export const refreshGoogleTokens = async (params: {
@@ -6,27 +7,17 @@ export const refreshGoogleTokens = async (params: {
   decryptedRefreshToken: string
 }): Promise<ResponseRefreshTokens> => {
   try {
-    const tokenResponse = await fetch('https://oauth2.googleapis.com/token', {
-      method: 'POST',
-      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
-      body: new URLSearchParams({
-        client_id: params.clientId,
-        client_secret: params.clientSecret,
-        refresh_token: params.decryptedRefreshToken,
-        grant_type: 'refresh_token',
-      }).toString(),
-    })
+    const google = new Google(params.clientId, params.clientSecret, '')
+    const tokens = await google.refreshAccessToken(params.decryptedRefreshToken)
 
-    const data = await tokenResponse.json()
-
-    if (!tokenResponse.ok || data.error_description) {
-      throw new Error(`Error refreshing token: ${data.error_description}`)
+    if (!tokens.accessToken()) {
+      throw new Error(`Invalid refresh token response type: ${JSON.stringify(tokens.data)}`)
     }
 
     return {
-      accessToken: data.access_token,
+      accessToken: tokens.accessToken(),
       refreshToken: params.decryptedRefreshToken,
-      expiresIn: data.expires_in,
+      expiresIn: tokens.accessTokenExpiresInSeconds(),
     }
   } catch (error) {
     throw error
