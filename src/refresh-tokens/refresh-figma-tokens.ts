75898f1301cd2b8151b7c6fdb7cadb063ee9522c
mikhailzakharov <michael.zacharov@ottofeller.com>
2025-01-22 15:00:29 +0100

    Revert "update refresh figma token by hands"
    This reverts commit e2373525d9bae716342865d1381505967b012959.

--- src/refresh-tokens/refresh-figma-tokens.ts
+++ src/refresh-tokens/refresh-figma-tokens.ts
@@ -1,3 +1,4 @@
+import {Bitbucket, Figma} from 'arctic'
 import {ResponseRefreshTokens} from 'sdk/types'
 
 export const refreshFigmaTokens = async (params: {
@@ -6,38 +7,17 @@ export const refreshFigmaTokens = async (params: {
   decryptedRefreshToken: string
 }): Promise<ResponseRefreshTokens> => {
   try {
-    const url = 'https://api.figma.com/v1/oauth/refresh'
-    const authHeader = Buffer.from(`${params.clientId}:${params.clientSecret}`).toString('base64')
+    const figma = new Figma(params.clientId, params.clientSecret, '')
+    const tokens = await figma.refreshAccessToken(params.decryptedRefreshToken)
 
-    const body = new URLSearchParams({
-      grant_type: 'refresh_token',
-      refresh_token: params.decryptedRefreshToken,
-    })
-
-    const response = await fetch(url, {
-      method: 'POST',
-      body: body.toString(),
-
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        Authorization: `Basic ${authHeader}`,
-      },
-    })
-
-    if (!response.ok) {
-      throw new Error(`HTTP Error: ${response.status} - ${response.statusText}`)
-    }
-
-    const data: {access_token: string; expires_in: number} = await response.json()
-
-    if (!data.access_token || !data.expires_in) {
-      throw new Error('Invalid refresh figma token')
+    if (!tokens.accessToken()) {
+      throw new Error(`Invalid refresh token response type: ${JSON.stringify(tokens.data)}`)
     }
 
     return {
-      accessToken: data.access_token,
+      accessToken: tokens.accessToken(),
       refreshToken: params.decryptedRefreshToken,
-      expiresIn: data.expires_in,
+      expiresIn: tokens.accessTokenExpiresInSeconds(),
     }
   } catch (error) {
     throw error
